174faed7fc64c2f2641714d2fe1fa740e4d04ea1
Masatoshi Nishimura <personal.trainer.bank@gmail.com>
2019-12-30 17:05:44 +0900

    app.record.edit.show　でも機能する様にした
    app.record.edit.show　でも機能する様にした。冗長・・・

--- drill-down.js
+++ drill-down.js
@@ -12,16 +12,15 @@
       PREFECTURE_APP_ID:157,//都道府県アプリ
       CITY_APP_ID:158//市区町村アプリ
   }
- 
-  kintone.events.on('app.record.create.show', function(event_show) {
-    let record = event_show.record;
-    //console.log(record.地方名_Main.value);
- 
+
+  kintone.events.on(['app.record.create.show','app.record.edit.show'], function(event_create_show) {
+    let record = event_create_show.record;
+    /* Create region query */
     let region_params = {
       "app": CONFIG.REGION_APP_ID,//地方アプリ
       "fields": ["地方名", "地方コード"]
     };
- 
+
     /* hidden Main App fields */
     kintone.app.record.setFieldShown('地方名_Main', false);
     kintone.app.record.setFieldShown('地方コード_Main', false);
@@ -32,7 +31,6 @@
  
     kintone.api(kintone.api.url('/k/v1/records', true), 'GET', region_params, function(region_resp) {
       /* Success */
-      //console.log(region_resp);
       let records_region = region_resp.records;
  
       /* Create Region Dropdown Options */
@@ -41,27 +39,127 @@
         let region_dropdown_option = {'label': element.地方名.value, 'value': element.地方コード.value, 'isDisabled':false};
         region_dropdown_options.push(region_dropdown_option);
       });
- 
-      /* Create Region kintoneUIComponent Objects */
-      let regionlabel = new kintoneUIComponent.Label({text: '地方名'});
-      let regiondropdown = new kintoneUIComponent.Dropdown({items: region_dropdown_options});
       
+      let regionCode_Main_value = record.地方コード_Main.value;
+      let prefectureCode_Main_value = record.都道府県コード_Main.value;
+      let cityCode_Main_value = record.市区町村コード_Main.value;
+
+      /* Create Region dropdown kintoneUIComponent Objects */
+      let regionlabel = new kintoneUIComponent.Label({text: '地方名'});
+      let regiondropdown = new kintoneUIComponent.Dropdown({items:region_dropdown_options,value:regionCode_Main_value});
+
       /* Reset All Dropdown on space */
-      kintone.app.record.getSpaceElement('space_region_main').innerHTML = "";
-      kintone.app.record.getSpaceElement('space_prefecture_main').innerHTML = "";
-      kintone.app.record.getSpaceElement('space_city_main').innerHTML = "";
+      kintone.app.record.getSpaceElement('space_region').innerHTML = "";
+      kintone.app.record.getSpaceElement('space_prefecture').innerHTML = "";
+      kintone.app.record.getSpaceElement('space_city').innerHTML = "";
       /* Setting Region Dropdown on space */
-      kintone.app.record.getSpaceElement('space_region_main').appendChild(regionlabel.render());
-      kintone.app.record.getSpaceElement('space_region_main').appendChild(regiondropdown.render());
+      kintone.app.record.getSpaceElement('space_region').appendChild(regionlabel.render());
+      kintone.app.record.getSpaceElement('space_region').appendChild(regiondropdown.render());
  
+      /* in case of app.record.edit.show */
+      /* if prefectureCode_Main_value is int , Create Prefecture dropdown kintoneUIComponent Objects */
+      if(!isNaN(prefectureCode_Main_value)){
+      
+      //Get 地方コード_Main records from 都道府県App
+      //create kintoneUIComponent items
+      //create kintoneUIComponent value by prefectureCode_Main_value
+      
+    　 let prefecture_params = {
+         "app": CONFIG.PREFECTURE_APP_ID,//都道府県アプリ
+         "query":"地方コード="+regionCode_Main_value,
+         "fields": ["地方コード","都道府県名","都道府県コード"]
+     　};
+      
+        kintone.api(kintone.api.url('/k/v1/records', true), 'GET', prefecture_params, function(prefecture_resp) {
+             
+          /* Success */
+          let records_prefecture = prefecture_resp.records;
+     
+          /* Create Prefecture Dropdown Options */
+          let prefecture_dropdown_options = [];
+          records_prefecture.forEach(function( element, index ){
+            let prefecture_dropdown_option = {'label': element.都道府県名.value, 'value': element.都道府県コード.value, 'isDisabled':false};
+            prefecture_dropdown_options.push(prefecture_dropdown_option);
+          });
+          
+          /* Create Prefecture kintoneUIComponent Objects */
+          let prefecturelabel = new kintoneUIComponent.Label({text: '都道府県名'});
+          let prefecturedropdown = new kintoneUIComponent.Dropdown({items: prefecture_dropdown_options,value:prefectureCode_Main_value});
+     
+          /* Reset Prefecture & city Dropdown on space */
+          kintone.app.record.getSpaceElement('space_prefecture').innerHTML = "";
+          kintone.app.record.getSpaceElement('space_city').innerHTML = "";
+          /* Setting Prefecture Dropdown on space */
+          kintone.app.record.getSpaceElement('space_prefecture').appendChild(prefecturelabel.render());
+          kintone.app.record.getSpaceElement('space_prefecture').appendChild(prefecturedropdown.render());
+
+          /* If change prefecture Dropdown , Insert value '都道府県名_Main' field . */
+          prefecturedropdown.on('change', function(value_prefecture) {
+            //Get_Main app all records include fields on space_prefecture
+            let records_Main_prefecture = kintone.app.record.get();
+    
+            for(let i=0; i<records_prefecture.length; i++){
+              if(value_prefecture == records_prefecture[i].都道府県コード.value){
+                records_Main_prefecture['record']['都道府県名_Main']['value'] = records_prefecture[i].都道府県名.value;
+                records_Main_prefecture['record']['都道府県コード_Main']['value'] = records_prefecture[i].都道府県コード.value;
+              }
+            }
+            kintone.app.record.set(records_Main_prefecture);
+          });
+        }, function(error) {
+        /* error */
+        console.log(error);
+        });
+
+      }
+
+      /* in case of app.record.edit.show */
+      /* if cityCode_Main_value is int , Create City dropdown kintoneUIComponent Objects */
+      if(!isNaN(cityCode_Main_value)){
+
+      //Get 市区町村コード_Main records from 市区町村App
+      //create kintoneUIComponent items
+      //create kintoneUIComponent value by cityCode_Main_value
+      
+        let city_params = {
+         "app": CONFIG.CITY_APP_ID,//市区町村アプリ
+         "query":"都道府県コード="+prefectureCode_Main_value,
+         "fields": ["都道府県コード","市区町村名","市区町村コード"]
+        };
+        
+         kintone.api(kintone.api.url('/k/v1/records', true), 'GET', city_params, function(city_resp) {
+           /* Success */
+           let records_city = city_resp.records;
+    
+          /* Create City Dropdown Options */
+          let city_dropdown_options = [];
+          records_city.forEach(function( element, index ){
+            let city_dropdown_option = {'label': element.市区町村名.value, 'value': element.市区町村コード.value, 'isDisabled':false};
+            city_dropdown_options.push(city_dropdown_option);
+          });
+    
+          /* Create City kintoneUIComponent Objects */
+          let citylabel = new kintoneUIComponent.Label({text: '市区町村名'});
+          let citydropdown = new kintoneUIComponent.Dropdown({items: city_dropdown_options, value:cityCode_Main_value });
+    
+          /* Reset City Dropdown on space */
+          kintone.app.record.getSpaceElement('space_city').innerHTML = "";
+          /* Setting City Dropdown on space */
+          kintone.app.record.getSpaceElement('space_city').appendChild(citylabel.render());
+          kintone.app.record.getSpaceElement('space_city').appendChild(citydropdown.render());
+    
+        }, function(error) {
+          /* error */
+          console.log(error);
+        });
+      
+      }
+      
       /* If change Region Dropdown , Insert value '地方名_Main' field . */
       regiondropdown.on('change', function(value_region) {
-        //Get_Main app all records include fields on space_region_main
+        //Get_Main app all records include fields on space_region
         let records_Main_region = kintone.app.record.get();
         
-        //console.log(records_Main_region.record);
-        //console.log("value",value_region);// This value is selected value .
-      
         for(let i=0; i<records_region.length; i++){
           if(value_region == records_region[i].地方コード.value){
             records_Main_region['record']['地方名_Main']['value'] = records_region[i].地方名.value;
@@ -70,28 +168,25 @@
         }
         kintone.app.record.set(records_Main_region);
       });
- 
     }, function(error) {
       // error
       console.log(error);
     });
  
-   kintone.events.on('app.record.create.change.地方コード_Main', function(event_region) {
-
+   kintone.events.on(['app.record.create.change.地方コード_Main','app.record.edit.change.地方コード_Main'], function(event_region) {
+    /* Success */
      /* Get 地方コード_Main field value when 地方コード_Main field change */
      let regioncode_Main = event_region.changes.field;
-     //console.log(regioncode_Main.value);
  
      /* Create prefecture query , use regioncode_Main.value */
      let prefecture_params = {
-       "app": CONFIG.PREFECTURE_APP_ID,//都道府県アプリ
-       "query":"地方コード="+regioncode_Main.value,
-       "fields": ["地方コード","都道府県名","都道府県コード"]
+         "app": CONFIG.PREFECTURE_APP_ID,//都道府県アプリ
+         "query":"地方コード="+regioncode_Main.value,
+         "fields": ["地方コード","都道府県名","都道府県コード"]
      };
- 
+
      kintone.api(kintone.api.url('/k/v1/records', true), 'GET', prefecture_params, function(prefecture_resp) {
        /* Success */
-       //console.log(prefecture_resp.records);
        let records_prefecture = prefecture_resp.records;
  
       /* Create Prefecture Dropdown Options */
@@ -100,25 +195,23 @@
         let prefecture_dropdown_option = {'label': element.都道府県名.value, 'value': element.都道府県コード.value, 'isDisabled':false};
         prefecture_dropdown_options.push(prefecture_dropdown_option);
       });
- 
+      
       /* Create Prefecture kintoneUIComponent Objects */
       let prefecturelabel = new kintoneUIComponent.Label({text: '都道府県名'});
       let prefecturedropdown = new kintoneUIComponent.Dropdown({items: prefecture_dropdown_options});
  
       /* Reset Prefecture & city Dropdown on space */
-      kintone.app.record.getSpaceElement('space_prefecture_main').innerHTML = "";
-      kintone.app.record.getSpaceElement('space_city_main').innerHTML = "";
+      kintone.app.record.getSpaceElement('space_prefecture').innerHTML = "";
+      kintone.app.record.getSpaceElement('space_city').innerHTML = "";
       /* Setting Prefecture Dropdown on space */
-      kintone.app.record.getSpaceElement('space_prefecture_main').appendChild(prefecturelabel.render());
-      kintone.app.record.getSpaceElement('space_prefecture_main').appendChild(prefecturedropdown.render());
+      kintone.app.record.getSpaceElement('space_prefecture').appendChild(prefecturelabel.render());
+      kintone.app.record.getSpaceElement('space_prefecture').appendChild(prefecturedropdown.render());
  
       /* If change prefecture Dropdown , Insert value '都道府県名_Main' field . */
       prefecturedropdown.on('change', function(value_prefecture) {
-        //Get_Main app all records include fields on space_prefecture_main
+        //Get_Main app all records include fields on space_prefecture
         let records_Main_prefecture = kintone.app.record.get();
-        //console.log(records_Main_prefecture.record);
-        //console.log("value",value_prefecture);// This value is selected value .
- 
+
         for(let i=0; i<records_prefecture.length; i++){
           if(value_prefecture == records_prefecture[i].都道府県コード.value){
             records_Main_prefecture['record']['都道府県名_Main']['value'] = records_prefecture[i].都道府県名.value;
@@ -135,22 +228,20 @@
  
     });
     
-    kintone.events.on('app.record.create.change.都道府県コード_Main', function(event_prefecture) {
-
+    kintone.events.on(['app.record.create.change.都道府県コード_Main','app.record.edit.change.都道府県コード_Main'], function(event_prefecture) {
+    /* Success */
      /* Get 都道府県コード_Main value when 都道府県コード_Main change */
      let prefecturecode_Main = event_prefecture.changes.field;
-     //console.log(prefecturecode_Main.value);
 
-       /* Create city query , use prefecturecode_Main.value */
+     /* Create city query , use prefecturecode_Main.value */
      let city_params = {
-       "app": CONFIG.CITY_APP_ID,//市区町村アプリ
-       "query":"都道府県コード="+prefecturecode_Main.value,
-       "fields": ["都道府県コード","市区町村名","市区町村コード"]
+         "app": CONFIG.CITY_APP_ID,//市区町村アプリ
+         "query":"都道府県コード="+prefecturecode_Main.value,
+         "fields": ["都道府県コード","市区町村名","市区町村コード"]
      };
 
      kintone.api(kintone.api.url('/k/v1/records', true), 'GET', city_params, function(city_resp) {
        /* Success */
-       //console.log(city_resp.records);
        let records_city = city_resp.records;
 
       /* Create City Dropdown Options */
@@ -165,18 +256,16 @@
       let citydropdown = new kintoneUIComponent.Dropdown({items: city_dropdown_options});
 
       /* Reset City Dropdown on space */
-      kintone.app.record.getSpaceElement('space_city_main').innerHTML = "";
+      kintone.app.record.getSpaceElement('space_city').innerHTML = "";
       /* Setting City Dropdown on space */
-      kintone.app.record.getSpaceElement('space_city_main').appendChild(citylabel.render());
-      kintone.app.record.getSpaceElement('space_city_main').appendChild(citydropdown.render());
+      kintone.app.record.getSpaceElement('space_city').appendChild(citylabel.render());
+      kintone.app.record.getSpaceElement('space_city').appendChild(citydropdown.render());
 
       /* If change city Dropdown , Insert value '市区町村名_Main' field . */
       citydropdown.on('change', function(value_city) {
-        //Get_Main app all records include fields on space_city_main
+        //Get_Main app all records include fields on space_city
         let records_Main_city = kintone.app.record.get();
-        //console.log(records_Main_city.record);
-        //console.log("value",value_city);// This value is selected value .
- 
+
         for(let i=0; i<records_city.length; i++){
           if(value_city == records_city[i].市区町村コード.value){
             records_Main_city['record']['市区町村名_Main']['value'] = records_city[i].市区町村名.value;
@@ -192,5 +281,12 @@
     });
 
    });
-  });
- })();
+  });/* app.record.create.show app.record.edit.show  */
+
+    kintone.events.on('app.record.detail.show', function(event) {
+        kintone.app.record.setFieldShown('地方コード_Main', false);
+        kintone.app.record.setFieldShown('都道府県コード_Main', false);
+        kintone.app.record.setFieldShown('市区町村コード_Main', false);
+    });
+
+})();
